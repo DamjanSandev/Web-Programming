80f27de8d62d24a7ced2a1169130c9c965e4a1de
223245 <frank.nords@protonmail.com>
2024-11-11 12:06:48 +0100

    Lab2 Additional

---
+++ src/main/java/mk/ukim/finki/wp/lab/web/controller/EventBookingController.java
@@ -0,0 +1,18 @@
+package mk.ukim.finki.wp.lab.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/eventBooking")
+public class EventBookingController {
+    @PostMapping("/confirm")
+    public String confirmBooking(@RequestParam String eventName, @RequestParam int numTickets, Model model) {
+        model.addAttribute("eventName", eventName);
+        model.addAttribute("numTickets", numTickets);
+        return "bookingConfirmation";
+    }
+}
