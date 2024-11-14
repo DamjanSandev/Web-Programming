fa423b2eeb436217a3558554ea108cc9fcf3ffa6
dsand <frank.nords@protonmail.com>
2024-11-14 15:38:27 +0100

    Added Lab 2 exercise

--- src/main/java/mk/ukim/finki/wp/lab/web/controller/EventBookingController.java
+++ src/main/java/mk/ukim/finki/wp/lab/web/controller/EventBookingController.java
@@ -2,6 +2,7 @@ package mk.ukim.finki.wp.lab.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/eventBooking")
 public class EventBookingController {
+
     @PostMapping("/confirm")
-    public String confirmBooking(@RequestParam String eventName, @RequestParam int numTickets, Model model) {
-        model.addAttribute("eventName", eventName);
+    public String confirm(@RequestParam("eventName") String name, @RequestParam("numTickets") int numTickets, Model model) {
+        model.addAttribute("eventName", name);
         model.addAttribute("numTickets", numTickets);
         return "bookingConfirmation";
     }
