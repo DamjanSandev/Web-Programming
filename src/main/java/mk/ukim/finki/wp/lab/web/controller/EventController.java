a931bc5d70bf074841353ea13f150ebdc00c2b64
dsand <frank.nords@protonmail.com>
2024-12-18 13:39:36 +0100

    Added InMemoryAuthentication

--- src/main/java/mk/ukim/finki/wp/lab/web/controller/EventController.java
+++ src/main/java/mk/ukim/finki/wp/lab/web/controller/EventController.java
@@ -9,6 +9,7 @@ import mk.ukim.finki.wp.lab.service.CategoryService;
 import mk.ukim.finki.wp.lab.service.EventService;
 import mk.ukim.finki.wp.lab.service.LocationService;
 import org.h2.util.Cache;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -55,14 +56,14 @@ public class EventController {
         model.addAttribute("locations", locationService.findAll());
         return "listEvents";
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add")
     public String saveEvent(@RequestParam String name, @RequestParam String description, @RequestParam double popularityScore, @RequestParam Long locationId, @RequestParam Long categoryId) {
         eventService.save(name, description, popularityScore, locationId, categoryId);
         return "redirect:/events";
 
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/addEvent/{id}")
     public String editEvent(@PathVariable Long id, Model model) {
         Optional<Event> event = this.eventService.findById(id);
@@ -78,14 +79,14 @@ public class EventController {
         model.addAttribute("categories", categories);
         return "add-event";
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/addEvent")
     public String addEvent(Model model) {
         model.addAttribute("locations", locationService.findAll());
         model.addAttribute("categories", categoryService.listAll());
         return "add-event";
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/update/{id}")
     public String updateEvent(@PathVariable Long id,
                               @RequestParam String name,
@@ -97,7 +98,7 @@ public class EventController {
         eventService.updateEvent(id, name, description, popularityScore, locationId, categoryId);
         return "redirect:/events";
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @Transactional
     @GetMapping("/delete/{id}")
     public String deleteEvent(@PathVariable Long id, Model model) {
